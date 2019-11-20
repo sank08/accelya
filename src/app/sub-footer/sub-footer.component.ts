34b4bcfd24cb5e555660955aa1bba21ad8126273
sank08 <57935432+sank08@users.noreply.github.com>
2019-11-20 23:41:04 +0530

    added sub footer links and css changes

--- src/app/sub-footer/sub-footer.component.ts
+++ src/app/sub-footer/sub-footer.component.ts
@@ -6,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sub-footer.component.css']
 })
 export class SubFooterComponent implements OnInit {
-
+  categoryList = [
+    "Graphics","Addons","Buttons","Infographics","PSDs","Fonts","Forms","Graphs","Icons","Textures","GUI","Logos","Templates","Patterns","navigation","Vectors","Themes","Backgrounds","UI Kits","Business Cards"
+  ]
   constructor() { }
 
   ngOnInit() {
